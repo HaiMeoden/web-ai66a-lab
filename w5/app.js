17dec061f26278d43703a453a432a0e34c5ecc64
HaiMeoden <hai234anh247@gmail.com>
2026-09-03 17:33:09 +0700

    w5 async fetchusers

--- w5/app.js
+++ w5/app.js
@@ -1,33 +1,36 @@
-async function greet () {
-  return "hello world";
-  
-}
-//console.log(greet());
-//c1
-function getData1() {
-  let text = "";
-  greet().then((response) => {
-    text = response;
+function renderUsers(users) {
+  let tableBody = document.querySelector("#user-table tbody");
+  // show users on UI
+  users.forEach((user) => {
+    const row = document.createElement("tr");
+    row.innerHTML = `
+      <td>${user.id}</td>
+      <td>${user.name}</td> 
+      <td>${user.phone}</td>
+      <td>${user.email.toLowerCase()}</td>
+      <td>${user.website}</td>
+      <td>${user.address.street + "-" + user.address.city}</td>`;
+
+    tableBody.appendChild(row);
   });
-  return text;
 }
-console.log(getData1());
 
-//c2
-async function getData2() {
-  const text =  await greet();
-  return text;
-}
-console.log(getData2());
-
-
-function fetchUsers() {
+// function fetchUsers() {
   
-  fetch("https://jsonplaceholder.typicode.com/users")
-    .then((response) => {return response.json();})
-    .then((users) => {
-      console.log(users);
-    });
-}
+//   fetch("https://jsonplaceholder.typicode.com/users")
+//     .then((response) => {return response.json();})
+//     .then((users) => {
+//       renderUsers(users);
+//     });
+// }
 
-fetchUsers();
+// rewrite the fetchUsers function using async/await
+async function fetchUsers() {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const users = await response.json();
+    renderUsers(users);
+  } catch (error) {
+    console.error("Error fetching users:", error);
+  }
+}
